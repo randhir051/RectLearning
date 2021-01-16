88ea8a89df5b857a8c6cd71f14aada35ed764e9f
Randhir <randhirsingh051@gmail.com>
2021-01-16 11:25:50 +0530

    In-Line styling Can enable dynamic Styling

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import getURL from "./urls";
 
-var title = "Favourites";
+const customStyle = {
+  color: "blue",
+  fontSize: "60px",
+  "text-align": "center"
+};
+
+// Changing on the fly
+customStyle.color = "gray";
+
+var title = "Favourite";
 ReactDOM.render(
   <div>
     <h1 className="heading">{title}</h1>
+    <h1 style={customStyle}>FOODS</h1>
     <div>
-      <img src={getURL(1)} alt="Banana" />
-      <img src={getURL(2)} alt="Cut Banana" />
-      <img src={getURL(3)} alt="Dolphin Banana" />
+      <img className="food-img" src={getURL(1)} alt="Banana" />
+      <img className="food-img" src={getURL(2)} alt="Cut Banana" />
+      <img className="food-img" src={getURL(3)} alt="Dolphin Banana" />
     </div>
   </div>,
   document.getElementById("root")
